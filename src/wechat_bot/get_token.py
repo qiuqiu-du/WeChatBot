67bf5e275bd5b7ba6c65fe4016e5b2c890235db5
qiuqiu-du <qiuqiudu@protonmail.com>
2025-03-25 16:19:25 +0800

    返回token

---
+++ src/wechat_bot/get_token.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "http://127.0.0.1:2531/v2/api/tools/getTokenId"
+
+payload={}
+headers = {}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
