961bd2958825ba4572ae4f74f392ea8db024f88f
qiuqiu-du <qiuqiudu@protonmail.com>
2025-04-12 14:31:48 +0800

    Read the configuration from the config.json file

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,5 +1,6 @@
 import os
 from dataclasses import dataclass
+import json
 
 @dataclass
 class WeChatConfig:
@@ -18,3 +19,13 @@ class WeChatConfig:
     reply_with_at: bool = True
 
     default_prompt: str = "你是一个人类，不要说自己是机器人，自然地和我在微信上对话(符合人们回复微信的习惯，不发大段的文字，简短直接，语句停顿只用逗号)"
+
+
+    @classmethod
+    def from_json(cls, path: str = "config.json"):
+        # 源码运行，从当前文件目录寻找 config.json
+        path = os.path.join(os.path.dirname(__file__), path)
+
+        with open(path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        return cls(**data)
