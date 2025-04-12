05d64875857e7b74e54ddc1abba57514d53656d1
qiuqiu-du <qiuqiudu@protonmail.com>
2025-04-12 14:29:37 +0800

    Refactored as a Method

--- src/wechat_bot/get_token.py
+++ src/wechat_bot/get_token.py
@@ -1,10 +1,11 @@
-import requests
+def gettoken():
+    import requests
+    url = "http://127.0.0.1:2531/v2/api/tools/getTokenId"
+    payload = {}
+    headers = {}
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.text
 
-url = "http://127.0.0.1:2531/v2/api/tools/getTokenId"
+gettoken()
 
-payload={}
-headers = {}
 
-response = requests.request("POST", url, headers=headers, data=payload)
-
-print(response.text)
